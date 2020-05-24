5651cb21e6c06c41ae638e48f915684f6a2f4f57
mcDevittMaya5 <iammaya@live.ie>
2020-05-24 22:27:24 +0100

    changes

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,16 +2,61 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
+    value: this.props.value,
+    // tags: [],
   };
+
+  // constructor() {
+  //   super();
+  //   this.handleIncrement = this.handleIncrement.bind(this);
+  //kinda out of date and looks gross
+  // }
+
+  //this behaves differently in javascript it can ref different things
+  handleIncrement = () => {
+    //using an arrow function
+    // console.log("increnemnt clicked", this);
+    //obj.method();
+    //function();
+    //we dont have access to this use bind method
+    this.setState({ count: this.state.value + 1 });
+    //setState() scheduals a render method t happens in the FuTuRe
+  };
+
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
+
+    return (
+      <ul>
+        <li>
+          {this.state.tags.map((tag) => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </li>
+      </ul>
+    );
+  }
 
   render() {
     return (
-      <React.Fragment>
-        <span>{this.formatCount()}</span>
-        <button>Increment</button>
-      </React.Fragment>
+      <div>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+        {/* {this.state.tags.length === 0 && "please create a new tag"} */}
+        {/* {this.renderTags()}; */}
+      </div>
     );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
   }
 
   formatCount() {
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Counter from "./components/counter";
-
-ReactDOM.render(<Counter />, document.getElementById("root"));
+import Counters from "./components/counters";
+import "bootstrap/dist/css/bootstrap.css";
+ReactDOM.render(<Counters />, document.getElementById("root"));
+//rendering a single component but we want a tree of components
